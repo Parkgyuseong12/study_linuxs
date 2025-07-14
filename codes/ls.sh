eebd052348da6c3761b0fd05546e37235e2550fc
Parkgyuseong12 <qkrrbtjd12@naver.com>
2025-07-14 18:02:15 +0900

    두번째 연습

---
+++ codes/ls.sh
@@ -0,0 +1,20 @@
+[gyuseong@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+[gyuseong@localhost ~]$ pwd
+/home/gyuseong
+[gyuseong@localhost ~]$ cd /home/gyuseong/Music
+[gyuseong@localhost Music]$ ls
+[gyuseong@localhost Music]$ pwd
+/home/gyuseong/Music
+[gyuseong@localhost Music]$ cd /home/gyuseong
+[gyuseong@localhost ~]$ pwd
+/home/gyuseong
+[gyuseong@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+[gyuseong@localhost ~]$ cd /home/Pictures
+bash: cd: /home/Pictures: No such file or directory
+[gyuseong@localhost ~]$ cd /home/gyuseong/Pictures
+[gyuseong@localhost Pictures]$ ls
+[gyuseong@localhost Pictures]$ pwd
+/home/gyuseong/Pictures
+[gyuseong@localhost Pictures]$ 
