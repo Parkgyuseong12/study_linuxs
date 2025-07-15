1b61a7bbc9d0a61e48ae695484f971260e14795b
Parkgyuseong12 <qkrrbtjd12@naver.com>
2025-07-15 12:23:07 +0900

    두번째 올리기

---
+++ codes/10_powershells.sh
@@ -0,0 +1,34 @@
+PS C:\Users\Administrator> cd C:\
+PS C:\> cd C:\Develops
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:58                Downloads
+d-----      2025-07-15  오전 11:14                Vms
+
+
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:58                Downloads
+d-----      2025-07-15  오전 11:14                Vms
+
+
+PS C:\Develops>
